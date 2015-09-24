2f9633710bfdfe988cb757c7f5d92b90cd4f7fd2
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-24 09:58:04 +0200

    Replace " with ' in definitions of all strings in itsthisforthat.js tests

--- test/itsthisforthat.js
+++ test/itsthisforthat.js
@@ -1,41 +1,41 @@
-var assert = require("assert");
-var itsThisForThat = require("../lib/itsthisforthat")();
+var assert = require('assert');
+var itsThisForThat = require('../lib/itsthisforthat')();
 
-describe("itsthisforthat", function() {
-    describe("init", function () {
+describe('itsthisforthat', function() {
+    describe('init', function () {
         var defaultRequestOptions = {};
 
         before(function () {
             defaultRequestOptions = {
-                hostname: "itsthisforthat.com",
+                hostname: 'itsthisforthat.com',
                 port: 80,
-                basePath: "/api.php"
+                basePath: '/api.php'
             }
         });
 
-        it("should have correct default request options", function () {
-            assert.deepEqual(itsThisForThat.defaultRequestOptions, defaultRequestOptions, "default request options should have correct initial property values");
+        it('should have correct default request options', function () {
+            assert.deepEqual(itsThisForThat.defaultRequestOptions, defaultRequestOptions, 'default request options should have correct initial property values');
         });
 
-        it("should not override default request options", function () {
+        it('should not override default request options', function () {
             itsThisForThat.defaultRequestOptions = {
-                hostname: "itsthisforthat.us",
+                hostname: 'itsthisforthat.us',
                 port: 3000,
-                basePath: "/api.php"
+                basePath: '/api.php'
             };
 
-            assert.deepEqual(itsThisForThat.defaultRequestOptions, defaultRequestOptions, "default request options should not be overridden");
+            assert.deepEqual(itsThisForThat.defaultRequestOptions, defaultRequestOptions, 'default request options should not be overridden');
         });
     });
 
-    describe("functions", function() {
-        describe("#getIdea()", function() {
-            it("should respond with valid idea object", function(done) {
+    describe('functions', function() {
+        describe('#getIdea()', function() {
+            it('should respond with valid idea object', function(done) {
                 itsThisForThat.getIdea(function(error, idea) {
                     if(!error) {
-                        assert.notEqual(idea, undefined, "idea should not be undefined");
-                        assert.notEqual(idea.this, undefined, "idea.this should not be undefined");
-                        assert.notEqual(idea.that, undefined, "idea.that should not be undefined");
+                        assert.notEqual(idea, undefined, 'idea should not be undefined');
+                        assert.notEqual(idea.this, undefined, 'idea.this should not be undefined');
+                        assert.notEqual(idea.that, undefined, 'idea.that should not be undefined');
                     } else {
                         assert.throws(function() {
                             throw error;
@@ -45,19 +45,19 @@ describe("itsthisforthat", function() {
                 });
             });
 
-            it("should fail when called without arguments", function() {
+            it('should fail when called without arguments', function() {
                 assert.throws(function() {
                     itsThisForThat.getIdea();
                 }, Error);
             });
 
-            it("should not be overridden", function() {
+            it('should not be overridden', function() {
                 itsThisForThat.getIdea = function() {
-                    return "#getIdea()";
+                    return '#getIdea()';
                 };
 
                 assert.throws(function() {
-                    assert.notEqual(itsThisForThat.getIdea(), "#getIdea()", "#getIdea() should not be overridden");
+                    assert.notEqual(itsThisForThat.getIdea(), '#getIdea()', '#getIdea() should not be overridden');
                 }, Error);
             });
         });
