8d15d7edc8d297ab4ddc8822a7d78c6ecf2b90d5
Anatoliy Gatt <anatoliy.gatt@aol.com>
2017-09-06 22:37:28 +0200

    Prettify

--- test/itsthisforthat.js
+++ test/itsthisforthat.js
@@ -2,45 +2,55 @@ var assert = require('assert');
 var itsThisForThat = require('../lib/itsthisforthat')();
 
 describe('itsthisforthat', function () {
-    describe('init', function () {
-        let defaultRequestOptions = {};
+  describe('init', function () {
+    let defaultRequestOptions = {};
 
-        before(function () {
-            defaultRequestOptions = {
-                hostname: 'itsthisforthat.com',
-                port: 80,
-                basePath: '/api.php'
-            }
-        });
-
-        it('should have correct default request options', function () {
-            assert.deepEqual(itsThisForThat.defaultRequestOptions, defaultRequestOptions, 'default request options should have correct initial property values');
-        });
+    before(function () {
+      defaultRequestOptions = {
+        hostname: 'itsthisforthat.com',
+        port: 80,
+        basePath: '/api.php'
+      };
     });
 
-    describe('functions', function () {
-        describe('#getIdea()', function () {
-            function validateIdea(idea) {
-                assert.ok(idea, 'idea should not be undefined, null or empty');
-                assert.ok(idea.this, 'idea.this should not be undefined, null or empty');
-                assert.ok(idea.that, 'idea.that should not be undefined, null or empty');
-            }
-
-            it('should respond with valid idea object', function (done) {
-                itsThisForThat.getIdea(function (error, idea) {
-                    if (error) {
-                        return done(error);
-                    }
-                    validateIdea(idea);
-                    done();
-                });
-            });
-
-            it('should fail when called without arguments', function () {
-                assert.throws(function () {
-                    itsThisForThat.getIdea();
-                }, Error);
-            });
-        });
+    it('should have correct default request options', function () {
+      assert.deepEqual(
+        itsThisForThat.defaultRequestOptions,
+        defaultRequestOptions,
+        'default request options should have correct initial property values'
+      );
     });
+  });
+
+  describe('functions', function () {
+    describe('#getIdea()', function () {
+      function validateIdea(idea) {
+        assert.ok(idea, 'idea should not be undefined, null or empty');
+        assert.ok(
+          idea.this,
+          'idea.this should not be undefined, null or empty'
+        );
+        assert.ok(
+          idea.that,
+          'idea.that should not be undefined, null or empty'
+        );
+      }
+
+      it('should respond with valid idea object', function (done) {
+        itsThisForThat.getIdea(function (error, idea) {
+          if (error) {
+            return done(error);
+          }
+          validateIdea(idea);
+          done();
+        });
+      });
+
+      it('should fail when called without arguments', function () {
+        assert.throws(function () {
+          itsThisForThat.getIdea();
+        }, Error);
+      });
+    });
+  });
 });
