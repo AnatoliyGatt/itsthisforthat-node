8907d03bcf33c2505a9c45f64769f493e493eaef
Anatoliy Gatt <anatoliy.gatt@aol.com>
2017-09-07 11:52:01 +0200

    2.0.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @module index
  * @description Entry point for itsthisforthat-node module.
- * @version 2.0.0
+ * @version 2.0.1
  * @author Anatoliy Gatt [anatoliy.gatt@aol.com]
  * @copyright Copyright (c) 2015-2017 Anatoliy Gatt
  * @license MIT
